763541e52195abf3e9245599a79cd0c6f63b8a1f
Meryem Essalhi <meryem.essalhi@gmail.com>
2024-12-11 13:28:30 +0000

    Added collaboration form to the about me page

--- about/views.py
+++ about/views.py
@@ -1,15 +1,28 @@
 from django.shortcuts import render, get_object_or_404
 from django.views import generic
 from .models import About
+from .forms import CollaborateForm
+from django.contrib import messages
 
 # Create your views here.
 def about_me(request):
     """
     Renders the About page
     """
+    if request.method == "POST":
+             
+             collab_form = CollaborateForm(data=request.POST)
+             if collab_form.is_valid():
+                collab_form.save()
+                messages.add_message(request, messages.SUCCESS,'Collaboration request received! I endeavour to respond within 2 working days.'
+                 )
     about = About.objects.all().order_by('-updated_on').first()
+      
+    collab_form = CollaborateForm()
+    
     return render(
         request,
         "about/about.html",
-        {"about": about},
-    )
+        {"about": about,
+        "collab_form": collab_form,},
+            )
